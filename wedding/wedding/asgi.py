70209a233a5aa6fd0c825d84eff7c41e039f3361
momt99 <mohammad.omt99@yahoo.com>
2022-02-24 20:44:32 +0330

    Rename wedding to invitation

--- wedding/wedding/asgi.py
+++ wedding/wedding/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for invitation project.
+ASGI config for wedding project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'invitation.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wedding.settings')
 
 application = get_asgi_application()
