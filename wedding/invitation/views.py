e5e630f632d235a5b167bb1afee179dce4f795df
momt99 <mohammad.omt99@yahoo.com>
2022-02-24 21:16:09 +0330

    Add invitation view

--- wedding/invitation/views.py
+++ wedding/invitation/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.views.generic import View
 
 def index(request):
     return HttpResponse("Hello, world. You're at the invitation index.")
+
+class InvitationsView(View):
+
+    def get(self, request, id):
+        return HttpResponse(f"It's GET of {self.kwargs['id']}.")
+
+    def post(self, request, id):
+        return HttpResponse(f"It's POST of {self.kwargs['id']}.")
