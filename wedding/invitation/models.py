28a579858e81cc60ec739c46796b65b966be3e5c
momt99 <mohammad.omt99@yahoo.com>
2022-02-24 21:42:24 +0330

    Add invited person model

--- wedding/invitation/models.py
+++ wedding/invitation/models.py
@@ -1,3 +1,21 @@
+from django import forms
 from django.db import models
+from shortuuid.django_fields import ShortUUIDField
 
-# Create your models here.
+class InvitedPerson(models.Model):
+    SEX_CHOICES = [
+        ("m", "Male"),
+        ("f", "Female"),
+        ("o", "Other")
+    ]
+
+    id = ShortUUIDField(
+        length=4,
+        max_length=8,
+        alphabet="abcdef1234567890",
+        primary_key=True,
+    )
+    name = models.TextField()
+    sex = forms.ChoiceField(choices=SEX_CHOICES)
+    description = models.TextField()
+    visit_count = models.IntegerField(default=0)
